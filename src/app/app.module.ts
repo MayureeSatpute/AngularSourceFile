ffa5d9e16a4127f2ed0ea681029595d038416435
MayureeSatpute <52929606+MayureeSatpute@users.noreply.github.com>
2019-10-08 12:47:04 +0530

    Add files via upload
    NgIf src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgForDemoComponent } from './ng-for-demo/ng-for-demo.component';
+import { NgIfDemoComponent } from './ng-if-demo/ng-if-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgForDemoComponent,
-   
+    NgIfDemoComponent
   ],
   imports: [
     BrowserModule
